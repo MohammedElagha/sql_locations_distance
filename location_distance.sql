add040de9bbe07ca5a690ee8a5ab60cef87f5aeb
Mohammed El-Agha <dev.magha@gmail.com>
2019-01-06 16:53:23 +0300

    Create location_distance.sql

---
+++ location_distance.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+
+USE `database name`$$
+
+DROP FUNCTION IF EXISTS `LOCATION_DISTANCE`$$
+
+CREATE FUNCTION `LOCATION_DISTANCE`(latitude_1 DOUBLE, longitude_1 DOUBLE, latitude_2 DOUBLE, longitude_2 DOUBLE) RETURNS DOUBLE
+BEGIN
+   DECLARE theta DOUBLE;
+   DECLARE dist DOUBLE;
+   DECLARE miles DOUBLE;
+   DECLARE kms DOUBLE;
+   SET theta = longitude_1 - longitude_2;
+   SET dist = DEGREES(ACOS(SIN(RADIANS(latitude_1)) * SIN(RADIANS(latitude_2)) +  COS(RADIANS(latitude_1)) * COS(RADIANS(latitude_2)) * COS(RADIANS(theta))));
+   SET miles = dist * 60 * 1.1515;	
+   SET kms = miles * 1.609344);	
+   RETURN kms;
+END$$
+
+DELIMITER ;
